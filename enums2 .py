b27b103868ca2c185599be26d4186a755716693c
mrandrenascimento <159596002+mrandrenascimento@users.noreply.github.com>
2024-09-09 12:54:07 +0000

    Salvar

--- enums2 .py
+++ enums2 .py
@@ -5,21 +5,32 @@ class Sexo(Enum):
     FEMININO = "Feminino"
 
 class Setor(Enum):
+    FINANCEIRO = "Finaceiro"
     RECURSOS_HUMAMNOS = "Recursos Humanos"
+    VENDAS = "Vendas"
     MARKETING = "Marketing"
 
 class Funcionario:    
-    def __init__(self,id:int, nome:str, idade:int, salario:float, ) -> None:
-        pass
+    def __init__(self,id:int, nome:str, idade:int, salario:float, setor:Setor, sexo:Sexo) -> None:
+        self.id=id
+        self.nome=nome
+        self.idade=idade
+        self.salario=salario
+        self.setor = setor
+        self.sexo=sexo
+
     def __str__(self) -> str:
         """Equivale ao toString() do java."""
-        return(f"\nNome: {self.nome}"
+        return(f"\nID: {self.id}"
+               f"\nNome: {self.nome}"
                f"\nIdade: {self.idade}"
+               f"\nSalário: {self.salario}"
+               f"\nSetor: {self.setor.value}"
                f"\nSexo: {self.sexo.value}"
                )
     
-#Instanciando Classe Pessoa.
+#Instanciando Classe Funcionário.
 os.system("cls||clear")
 
-pessoa_p1 = Pessoa("Marta",22,Sexo.FEMININO)
-print(pessoa_p1)
+funcionario_1 = Funcionario(424,"João",23,1425.35,Setor.MARKETING, Sexo.MASCULINO)
+print(funcionario_1)
